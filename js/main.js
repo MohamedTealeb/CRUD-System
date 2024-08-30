2560a3ece2b0f85183322c052136f89a48f4a4ba
justmahmoud31 <justmahmoud3152003@gmail.com>
2024-08-30 23:30:43 +0300

    adding search

--- js/main.js
+++ js/main.js
@@ -1,37 +1,32 @@
-var productNameInput=document.getElementById('productName')
-var productCategotyInput=document.getElementById('productCategory')
-var producPriceInput=document.getElementById('productPrice')
-var productDescriptioneInput=document.getElementById('productDescription')
+var productNameInput = document.getElementById('productName');
+var productCategotyInput = document.getElementById('productCategory');
+var producPriceInput = document.getElementById('productPrice');
+var productDescriptioneInput = document.getElementById('productDescription');
 
-var productcontainer=[]
-if(localStorage.getItem('products')!=null){
-    productcontainer= JSON.parse( localStorage.getItem('products'))
+var productcontainer = [];
+if (localStorage.getItem('products') != null) {
+    productcontainer = JSON.parse(localStorage.getItem('products'));
 }
-// console.log("yoo");
 
-
-function addproduct(){
-   
-    var productobj={
-        name:productNameInput.value,
-        category:productCategotyInput.value,
-        price:producPriceInput.value,
-        description:productDescriptioneInput.value,
-    
-    }
-    productcontainer.push(productobj)
-    display()
-   localStorage.setItem('products',JSON.stringify(productcontainer))
-   clearInput()
-
+function addproduct() {
+    var productobj = {
+        name: productNameInput.value,
+        category: productCategotyInput.value,
+        price: producPriceInput.value,
+        description: productDescriptioneInput.value,
+    };
+    productcontainer.push(productobj);
+    display();
+    localStorage.setItem('products', JSON.stringify(productcontainer));
+    clearInput();
 }
-function display(){
 
-var cartona=``
-for(var i=0;i<productcontainer.length;i++){
-cartona+=`
+function display() {
+    var cartona = ``;
+    for (var i = 0; i < productcontainer.length; i++) {
+        cartona += `
 <tr>
-<td>${i+1}</td>
+<td>${i + 1}</td>
 <td>${productcontainer[i].name}</td>
 <td>${productcontainer[i].category}</td>
 <td>${productcontainer[i].price}</td>
@@ -39,40 +34,48 @@ cartona+=`
 <td><button onclick="deleteitem(${i})"  class="btn btn-danger btn-sm">Delete</button></td>
 <td><button class="btn btn-success btn-sm">Update</button></td>
 </tr>
-`
+`;
+    }
+    document.getElementById('tbody').innerHTML = cartona;
 }
-document.getElementById('tbody').innerHTML=cartona
 
+display();
+
+function clearInput() {
+    productNameInput.value = "";
+    productCategotyInput.value = "";
+    producPriceInput.value = "";
+    productDescriptioneInput.value = "";
 }
-display()
-function clearInput(){
-    productNameInput.value="";
-    productCategotyInput.value="";
-    producPriceInput.value="";
-    productDescriptioneInput.value="";
+
+function deleteitem(index) {
+    productcontainer.splice(index, 1);
+    localStorage.setItem('products', JSON.stringify(productcontainer));
+    display();
 }
-function deleteitem(Index){
-productcontainer.splice(Index,1)
-localStorage.setItem('products',JSON.stringify(productcontainer))
-display()
-}
-function search(){
-var search_5=document.getElementById('searchInput').value
-var box2=``
-for(var i=0;i<productcontainer.length;i++){
-    if(productcontainer[i].productNameInput.includes(search_5)){
-        // console.log("yes",i);
-        box2+=`
+
+function search() {
+    var searchInput = document.getElementById('searchInput').value.toLowerCase();
+    var searchResult = ``;
+
+    for (var i = 0; i < productcontainer.length; i++) {
+        if (productcontainer[i].name.toLowerCase().includes(searchInput)) {
+
+            var highlightedName = productcontainer[i].name.replace(
+                new RegExp(searchInput, 'gi'),
+                (match) => `<span class="highlight">${match}</span>`
+            );
+            searchResult += `
         <tr>
-<td>${i+1}</td>
-<td>${productcontainer[i].name}</td>
+<td>${i + 1}</td>
+<td>${highlightedName}</td>
 <td>${productcontainer[i].category}</td>
 <td>${productcontainer[i].price}</td>
 <td>${productcontainer[i].description}</td>
-<td><button onclick="deleteitem(${i})"  class="btn btn-danger btn-sm">Delete</button></td>
+<td><button onclick="deleteitem(${i})" class="btn btn-danger btn-sm">Delete</button></td>
 <td><button class="btn btn-success btn-sm">Update</button></td>
-</tr>`
+</tr>`;
+        }
     }
-}
-document.getElementById('tbody').innerHTML=box2
+    document.getElementById('tbody').innerHTML = searchResult;
 }
